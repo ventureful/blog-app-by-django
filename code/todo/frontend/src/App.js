fcbb96b941004707144473b5d8097c4a05853a21
ventureful <hades0122@outlook.com>
2022-03-26 05:38:13 -0700

    Use axios. Create getTodos() to fetch date from API

--- code/todo/frontend/src/App.js
+++ code/todo/frontend/src/App.js
@@ -1,37 +1,34 @@
 import React, { Component } from 'react';
-
-
-const list = [
-  {
-    "id":1,
-    "title":"Learn Django",
-    "body":"Understand how to create basic django powered sites and setup initial configuration"
-  },
-  {
-    "id":2,
-    "title":"Study Python builtins",
-    "body":"Understand how to use some of the more important Python builtin functions and methods"
-  },
-  {
-    "id":3,
-    "title":"Replace os.path with pathlib in repocapp",
-    "body":"Learn how to use the Python library pathlib"
-  }
-]
+import axios from 'axios';
 
 
 class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = { list };
+  state = {
+      todos: []
+  };
+
+  componentDidMount() {
+    this.getTodos();
   }
+
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        this.setState({ todos: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
-            <p>{item.body}</p>
+            <span>{item.body}</span>
           </div>
         ))}
       </div>
