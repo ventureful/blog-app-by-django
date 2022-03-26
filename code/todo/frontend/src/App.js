94bd78ea64f1d604ae87b991de68ae83b1363565
ventureful <hades0122@outlook.com>
2022-03-26 05:21:40 -0700

    Load list in app. Loop over items

--- code/todo/frontend/src/App.js
+++ code/todo/frontend/src/App.js
@@ -18,3 +18,26 @@ const list = [
     "body":"Learn how to use the Python library pathlib"
   }
 ]
+
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { list };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.list.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+
+export default App;
