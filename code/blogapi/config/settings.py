ecbba4014eceabfb44112f200b762c24d38998ac
ventureful <hades0122@outlook.com>
2022-03-26 12:38:32 -0700

    Update INSTALLED_APPS, define permission classes

--- code/blogapi/config/settings.py
+++ code/blogapi/config/settings.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd-party apps
+    'rest_framework',
+
     # Local
-    'posts',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
