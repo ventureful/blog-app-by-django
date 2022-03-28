25088e1b640dece36e43bbeb994efe3062e8633d
ventureful <hades0122@outlook.com>
2022-03-27 22:41:14 -0700

    Add allauth & user registration endpoint

--- code/blogapi/config/settings.py
+++ code/blogapi/config/settings.py
@@ -41,11 +41,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd-party apps
     'rest_framework',
     'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'dj_rest_auth',
+    'dj_rest_auth.registration',
 
     # Local
     'posts.apps.PostsConfig',
@@ -145,3 +150,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
