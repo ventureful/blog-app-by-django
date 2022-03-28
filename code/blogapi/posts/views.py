6639e21478a68aaa9ae5aaa6b67106a31848b426
ventureful <hades0122@outlook.com>
2022-03-28 11:49:36 -0700

    Consolidate List & Detail classes into viewsets

--- code/blogapi/posts/views.py
+++ code/blogapi/posts/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
@@ -14,6 +20,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
 
 
 class UserList(generics.ListCreateAPIView):
