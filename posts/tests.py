7efa63d22d1e9d2612b585f43bc60e63824f6170
ventureful <hades0122@outlook.com>
2022-06-09 17:07:18 -0700

    Fix per black

--- posts/tests.py
+++ posts/tests.py
@@ -8,11 +8,15 @@ class BlogTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Create a user
-        testuser1 = User.objects.create_user(username="testuser1", password="abc123")
+        testuser1 = User.objects.create_user(
+            username="testuser1", password="abc123"
+        )
         testuser1.save()
 
         # Create a blog post
-        test_post = Post.objects.create(author=testuser1, title="Blog title", body="Body content...")
+        test_post = Post.objects.create(
+            author=testuser1, title="Blog title", body="Body content..."
+        )
         test_post.save()
 
     def test_blog_content(self):
