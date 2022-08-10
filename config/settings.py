549f0e61af5b238b26175ed847e8fa8050c66b75
ventureful <hades0122@outlook.com>
2022-08-10 13:27:17 -0700

    Configure pagination. Resolves #41. Resolves #42

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 5,
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
