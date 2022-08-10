5be578f6504e8b87b1bbcdc41b9f99713f4282c4
ventureful <hades0122@outlook.com>
2022-08-10 15:43:57 -0700

    Add accounts & pages locations

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import tempfile
 import nox
 
 nox.options.sessions = "lint", "safety", "tests"
-locations = "config", "posts", "./noxfile.py"
+locations = "accounts", "config", "pages", "posts", "./noxfile.py"
 
 
 def install_with_constraints(session, *args, **kwargs):
