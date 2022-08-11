529d8eb43d58d1ca3fae9babeac0c455ebbddc4a
ventureful <hades0122@outlook.com>
2022-08-11 16:19:40 -0700

    Remove ViewSets

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,30 @@
 from django.contrib.auth import get_user_model
 from django.views.generic import ListView
-from rest_framework import viewsets
+from rest_framework import generics
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostList(generics.ListCreateAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
