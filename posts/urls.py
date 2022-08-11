f8e5d64f8bfcab73612a04681127a924806dbadc
ventureful <hades0122@outlook.com>
2022-08-11 16:17:38 -0700

    Remove routers for better test coverage

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path  # noqa: F401
-from rest_framework.routers import SimpleRouter
+from django.urls import path
 
-from .views import PostViewSet, UserViewSet
+from .views import PostDetail, PostList, UserDetail, UserList
 
-router = SimpleRouter()
-router.register("users", UserViewSet, basename="users")
-router.register("", PostViewSet, basename="posts")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("users/", UserList.as_view()),
+    path("users/<int:pk>/", UserDetail.as_view()),
+    path("", PostList.as_view(), name="post_list"),
+    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+]
