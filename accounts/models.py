2ff8bd93b9caa4b22f72835d6092ca27b56895bc
ventureful <hades0122@outlook.com>
2022-08-11 17:15:25 -0700

    Add profile_pic field to CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ class CustomUser(AbstractUser):
     )
     age = models.PositiveIntegerField(null=True, blank=True)
     bio = models.TextField("Bio", blank=True)
+    profile_pic = models.ImageField(upload_to="profile_pics/", blank=True)
 
     def __str__(self):
         return self.username
